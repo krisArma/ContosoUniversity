0ac83c1634d8450cd1faf0c9fe5a91a562b9c2b9
krisArma <dansena13@gmail.com>
2017-06-20 16:13:23 +0700

    adding data in students

--- ContosoUniversity/DAL/SchoolInitializer.cs
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -22,6 +22,7 @@ namespace ContosoUniversity.DAL
                 new Student{FirstMidName="Peggy",LastName="Justice",EnrollmentDate=DateTime.Parse("2001-09-01")},
                 new Student{FirstMidName="Laura",LastName="Norman",EnrollmentDate=DateTime.Parse("2003-09-01")},
                 new Student{FirstMidName="Nino",LastName="Olivetto",EnrollmentDate=DateTime.Parse("2005-09-01")}
+                new Student{FirstMidName="Daniel",LastName="Suroso",EnrollmentDate=DateTime.Parse("2003-09-01")}
             };
 
             studets.ForEach(s => context.Students.Add(s));
